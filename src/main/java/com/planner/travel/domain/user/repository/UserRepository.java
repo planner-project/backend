64595eb5e0e9a117fa5b585082f2841392a23e9a
sieunnnn <wldsmtldsm65@gmail.com>
2024-04-02 17:23:11 +0900

    Setting: user repository 작성 (#3)

---
+++ src/main/java/com/planner/travel/domain/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.planner.travel.domain.user.repository;
+
+import com.planner.travel.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
