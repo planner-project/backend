3dcb7b59f634c9f502a118429f8d186818da5903
sieunnnn <wldsmtldsm65@gmail.com>
2024-04-09 21:59:05 +0900

    Feat: 랜덤넘버 유틸 틀 생성 (#3)

---
+++ src/main/java/com/planner/travel/global/util/RandomNumberUtil.java
@@ -0,0 +1,8 @@
+package com.planner.travel.global.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandomNumberUtil {
+    // 여기에 6 자리 랜덤 넘버를 만들어 주는 메서드를 생성해주세요!
+}
