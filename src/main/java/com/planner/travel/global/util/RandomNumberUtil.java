930ea134641c4d3c4f7a187d308e0c10141fbce8
sieunnnn <wldsmtldsm65@gmail.com>
2024-04-18 15:32:38 +0900

    Refac: 랜덤 함수 따로 분리 (#3)

--- src/main/java/com/planner/travel/global/util/RandomNumberUtil.java
+++ src/main/java/com/planner/travel/global/util/RandomNumberUtil.java
@@ -2,7 +2,13 @@ package com.planner.travel.global.util;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Random;
+
 @Component
 public class RandomNumberUtil {
-    // 여기에 6 자리 랜덤 넘버를 만들어 주는 메서드를 생성해주세요!
+    public Long set() {
+        Random random = new Random();
+        long randomNumber = random.nextLong(9000) + 1000;
+        return randomNumber;
+    }
 }
