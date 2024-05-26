c369c45a329fba0c1ba6928e7310b856314c7991
sieunnnn <wldsmtldsm65@gmail.com>
2024-05-26 15:11:52 +0900

    Feat: 그룹 멤버인지 아닌지 확인하는 기능 구현 (#18)

--- src/main/java/com/planner/travel/domain/group/controller/GroupMemberController.java
+++ src/main/java/com/planner/travel/domain/group/controller/GroupMemberController.java
@@ -2,6 +2,7 @@ package com.planner.travel.domain.group.controller;
 
 import com.planner.travel.domain.group.dto.response.GroupMemberResponse;
 import com.planner.travel.domain.group.query.GroupMemberQueryService;
+import com.planner.travel.domain.group.service.GroupMemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +16,7 @@ import java.util.List;
 @RequestMapping(value = "/api/v1/users")
 @RequiredArgsConstructor
 public class GroupMemberController {
+    private final GroupMemberService groupMemberService;
     private final GroupMemberQueryService groupMemberQueryService;
 
     @GetMapping(value = "/{userId}/planners/{plannerId}/group")
@@ -22,5 +24,12 @@ public class GroupMemberController {
         List<GroupMemberResponse> groupMemberResponses = groupMemberQueryService.findGroupMembersByPlannerId(plannerId);
 
         return ResponseEntity.ok(groupMemberResponses);
+    }
+
+    @GetMapping(value = "/{userId}/planners/{plannerId}/group/check")
+    public ResponseEntity<?> isGroupMember(@PathVariable("userId") Long userId, @PathVariable("plannerId") Long plannerId) {
+        boolean isGroupMember = groupMemberService.isGroupMember(userId, plannerId);
+
+        return ResponseEntity.ok(isGroupMember);
     }
 }
