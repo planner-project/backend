24913a0b841786a6b98646e387d118bae1eb796f
sieunnnn <wldsmtldsm65@gmail.com>
2024-05-29 19:56:56 +0900

    Fix: 웹소켓 버그 수정

--- src/main/java/com/planner/travel/global/jwt/JWTAuthenticationFilter.java
+++ src/main/java/com/planner/travel/global/jwt/JWTAuthenticationFilter.java
@@ -3,12 +3,14 @@ package com.planner.travel.global.jwt;
 import com.planner.travel.global.jwt.token.TokenAuthenticator;
 import com.planner.travel.global.jwt.token.TokenExtractor;
 import com.planner.travel.global.jwt.token.TokenValidator;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -41,10 +43,24 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
         String accessToken = tokenExtractor.getAccessTokenFromHeader(request);
 
         if (accessToken != null) {
-            tokenValidator.validateAccessToken(accessToken);
-            tokenAuthenticator.getAuthenticationUsingToken(accessToken);
+            try {
+                tokenValidator.validateAccessToken(accessToken);
+                tokenAuthenticator.getAuthenticationUsingToken(accessToken);
+
+            } catch (ExpiredJwtException e) {
+                response.setStatus(HttpStatus.UNAUTHORIZED.value());
+                setResponse(response, "TOKEN_01");
+
+            }
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void setResponse(HttpServletResponse response, String errorCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter().println(
+                "{\"errorCode\" : \"" + errorCode + "\"}"
+        );
     }
 }
