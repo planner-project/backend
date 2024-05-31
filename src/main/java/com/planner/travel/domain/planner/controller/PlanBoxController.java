3bcc7b4d7a04b56ea9dac72f18595c3e14c46c09
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-01 01:34:12 +0900

    Docs: plan, planbox api 명세 작성 (#28)

--- src/main/java/com/planner/travel/domain/planner/controller/PlanBoxController.java
+++ src/main/java/com/planner/travel/domain/planner/controller/PlanBoxController.java
@@ -10,7 +10,6 @@ import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.Map;
@@ -22,18 +21,12 @@ public class PlanBoxController {
     private final PlanBoxService planBoxService;
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    // 웹소켓 주소 명세는 노션을 참고해주세요.
-    // 플랜박스 코드 다 작성하면 웹소켓 테스트 하는 법 알려드리겠습니다!
-    // 플랜박스를 CUD 할 때는 전체 planBox 리스트를 다시 보내주세요. 관련 서비스는 PlanBoxQueryService 에 있습니다.
-
-    @MessageMapping(value = "/planner/{plannerId}/create") // <- 프론트가 보내는 주소 입니다.
+    @MessageMapping(value = "/planner/{plannerId}/create")
     public void createDate(@DestinationVariable Long plannerId, @RequestBody PlanBoxCreateRequest request) {
-        // 서비스를 작성해주세요
         planBoxService.create(request, plannerId);
 
-        // 결과값은 항상 같습니다.
         simpMessagingTemplate.convertAndSend("/sub/planner/" + plannerId,
-                Map.of("type", "create-planBox", "message", planBoxService.getAllPlanBox(plannerId)) // <- 해당 주소를 구독하는 모든 사람이 CUD 과정을 볼 수 있습니다.
+                Map.of("type", "create-planBox", "message", planBoxService.getAllPlanBox(plannerId))
         );
     }
 
@@ -45,14 +38,13 @@ public class PlanBoxController {
                 Map.of("type", "update-planBox", "message", planBoxService.getAllPlanBox(plannerId))
         );
     }
+
     @MessageMapping(value = "/planner/{plannerId}/delete/{planBoxId}")
     public void deleteDate(@DestinationVariable("plannerId") Long plannerId, @DestinationVariable("planBoxId") Long planBoxId) {
         planBoxService.delete(planBoxId);
 
     simpMessagingTemplate.convertAndSend("/sub/planner/" + plannerId,
-                Map.of("type", "delete-planBox", "message", planBoxService.getAllPlanBox(plannerId)) // <- 해당 주소를 구독하는 모든 사람이 CUD 과정을 볼 수 있습니다.
+                Map.of("type", "delete-planBox", "message", planBoxService.getAllPlanBox(plannerId))
         );
     }
-
-
 }
--- src/main/java/com/planner/travel/domain/planner/controller/PlanController.java
+++ src/main/java/com/planner/travel/domain/planner/controller/PlanController.java
@@ -19,34 +19,28 @@ public class PlanController {
     private final PlanService planService;
     private final SimpMessagingTemplate simpMessagingTemplate;
 
-    @MessageMapping(value = "/planner/{plannerId}/planBox/{planBoxId}/create") // <- 프론트가 보내는 주소 입니다.
+    @MessageMapping(value = "/planner/{plannerId}/planBox/{planBoxId}/create")
     public void createDate(@DestinationVariable("plannerId") Long plannerId, @DestinationVariable("planBoxId") Long planBoxId, @RequestBody PlanCreateRequest request) {
-        // 서비스를 작성해주세요
         planService.create(request, planBoxId);
 
-        // 결과값은 항상 같습니다.
         simpMessagingTemplate.convertAndSend("/sub/planner/" + plannerId,
-                Map.of("type", "create-plan", "message", planService.getAllPlan(planBoxId)) //
+                Map.of("type", "create-plan", "message", planService.getAllPlan(planBoxId))
         );
     }
 
-    @MessageMapping(value = "/planner/{plannerId}/planBox/{planBoxId}/update/{planId}") // <- 프론트가 보내는 주소 입니다.
-    public void updateDate( @DestinationVariable Long plannerId,@DestinationVariable("planBoxId") Long planBoxId,@DestinationVariable("planId") Long planId,  @RequestBody PlanUpdateRequest request) {
-        // 서비스를 작성해주세요
+    @MessageMapping(value = "/planner/{plannerId}/planBox/{planBoxId}/update/{planId}")
+    public void updateDate(@DestinationVariable Long plannerId, @DestinationVariable("planBoxId") Long planBoxId, @DestinationVariable("planId") Long planId, @RequestBody PlanUpdateRequest request) {
         planService.update(request, planId);
 
-        // 결과값은 항상 같습니다.
         simpMessagingTemplate.convertAndSend("/sub/planner/" + plannerId,
                 Map.of("type", "update-plan", "message", planService.getAllPlan(planBoxId))
         );
     }
 
-    @MessageMapping(value = "/planner/{plannerId}/planBox/{planBoxId}/delete/{planId}") // <- 프론트가 보내는 주소 입니다.
-    public void deleteDate( @DestinationVariable("plannerId") Long plannerId,@DestinationVariable("planBoxId") Long planBoxId,@DestinationVariable("planId") Long planId) {
-        // 서비스를 작성해주세요
+    @MessageMapping(value = "/planner/{plannerId}/planBox/{planBoxId}/delete/{planId}")
+    public void deleteDate(@DestinationVariable("plannerId") Long plannerId, @DestinationVariable("planBoxId") Long planBoxId, @DestinationVariable("planId") Long planId) {
         planService.delete(planId);
 
-        // 결과값은 항상 같습니다.
         simpMessagingTemplate.convertAndSend("/sub/planner/" + plannerId,
                 Map.of("type", "delete-plan", "message", planService.getAllPlan(planBoxId))
         );
