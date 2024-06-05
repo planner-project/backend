83e60b8f1a5aa1104aab43b49a3aeaf607ad8191
sieunnnn <wldsmtldsm65@gmail.com>
2024-06-05 16:12:12 +0900

    Fix: 유저 정보 반환에 이메일 추가 (#33)

--- src/test/java/com/planner/travel/user/UserInfoControllerTest.java
+++ src/test/java/com/planner/travel/user/UserInfoControllerTest.java
@@ -45,10 +45,12 @@ public class UserInfoControllerTest {
     public void testGetUserInfo() throws Exception {
         String fakeToken = "Bearer generated.access.token";
         Long expectedUserId = 1L;
-        UserInfoResponse expectedUserInfo = new UserInfoResponse(1L,
+        UserInfoResponse expectedUserInfo = new UserInfoResponse(
+                1L,
                 "시은",
                 1234L,
                 LocalDate.parse("1996-11-20"),
+                "wldsmtldsm65@gmail.com",
                 "",
                 false,
                 Sex.NONE
@@ -64,6 +66,9 @@ public class UserInfoControllerTest {
                 .andExpect(jsonPath("$.userId").value(expectedUserInfo.userId()))
                 .andExpect(jsonPath("$.nickname").value(expectedUserInfo.nickname()))
                 .andExpect(jsonPath("$.userTag").value(expectedUserInfo.userTag()))
+                .andExpect(jsonPath("$.birthday").value(expectedUserInfo.birthday().toString()))
+                .andExpect(jsonPath("$.email").value(expectedUserInfo.email()))
+                .andExpect(jsonPath("$.profileImgUrl").value(expectedUserInfo.profileImgUrl()))
                 .andExpect(jsonPath("$.isBirthday").value(expectedUserInfo.isBirthday()))
                 .andExpect(jsonPath("$.sex").value(expectedUserInfo.sex().toString()))
 
@@ -75,6 +80,7 @@ public class UserInfoControllerTest {
                                 PayloadDocumentation.fieldWithPath("nickname").description("유저 닉네임"),
                                 PayloadDocumentation.fieldWithPath("userTag").description("유저 태그"),
                                 PayloadDocumentation.fieldWithPath("birthday").description("유저 생년월일"),
+                                PayloadDocumentation.fieldWithPath("email").description("유저 이메일"),
                                 PayloadDocumentation.fieldWithPath("profileImgUrl").description("프로필 이미지 url"),
                                 PayloadDocumentation.fieldWithPath("isBirthday").description("유저 생일 여부"),
                                 PayloadDocumentation.fieldWithPath("sex").description("유저 성별")
